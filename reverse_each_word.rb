615d83dea8b93349e1a3a78efc4f8d59fb760cde
ebuchheim <emma.buchheim@gmail.com>
2019-01-15 10:28:49 -0800

    remove irrelevant code comment

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
     array = sentence.split(/(\s+)/)
     reversed = []
-    #so now I have an array of letters, spaces, & punctuation
     array.collect do |word|
         reversed_word = word.reverse
         reversed.push(reversed_word)
